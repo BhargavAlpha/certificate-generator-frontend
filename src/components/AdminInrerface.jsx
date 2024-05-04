908ecf2427568d698e37151ad8b6fa9f4164e036
Bhargav <bhargavvenkat515@gmail.com>
2024-05-04 22:58:56 +0530

    Added form validation

--- src/components/AdminInrerface.jsx
+++ src/components/AdminInrerface.jsx
@@ -14,6 +14,7 @@ const AdminInterface = () => {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
   const generateCertificateId = () => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let certificateId = '';
@@ -25,17 +26,21 @@ const AdminInterface = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-      const certificateId = generateCertificateId();
-      formData.certificateId = certificateId;
-      fillPDF(formData);
-    };
-    
+    if (!formData.name || !formData.email || !formData.course || !formData.date) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    const certificateId = generateCertificateId();
+    formData.certificateId = certificateId;
+    fillPDF(formData);
+  };
 
   return (
     <div className='form-div'>
       <form className="form" onSubmit={handleSubmit}>
-       <h2>Admin Interface</h2>
-        <input type="text" name="name" placeholder="Name" className="form-input"  value={formData.name} onChange={handleChange} />
+        <h2>Generate Certificate</h2>
+        <input type="text" name="name" placeholder="Name" className="form-input" value={formData.name} onChange={handleChange} />
         <input type="text" name="email" placeholder="Students's Email" className="form-input" value={formData.email} onChange={handleChange} />
         <input type="text" name="course" placeholder="Course" className="form-input" value={formData.course} onChange={handleChange} />
         <input type="date" name="date" placeholder="Date of Approval" className="form-input" value={formData.date} onChange={handleChange} />
